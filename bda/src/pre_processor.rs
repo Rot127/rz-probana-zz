6d284eadca2e02812ada33b47e7c70801926638b
Rot127 <unisono@quyllur.org>
2023-12-01 00:04:30 -0500

    Add iCFG structs

--- bda/src/pre_processor.rs
+++ bda/src/pre_processor.rs
@@ -2,12 +2,5 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 
 // Todo
-//
-// - Print info adding additional malloc symbols (in Rizin for Hexagon and big binaries)
-// - Translate graph
-//   - Check for malloc in graph
-// - Check for malloc in arguments
-// - Warn if no malloc given with sleep
-// - Check and mark loops/recursions.
-//   - Resolve loops?
-//
+// Trigger translate graph
+// Trigger loop resolvement
