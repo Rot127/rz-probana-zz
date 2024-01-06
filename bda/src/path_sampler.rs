1fc3311628b6336282ebb4238e70b74ae055bf11
Rot127 <unisono@quyllur.org>
2024-01-06 11:39:05 -0500

    Add note design of path sampler.

--- bda/src/path_sampler.rs
+++ bda/src/path_sampler.rs
@@ -1,2 +1,10 @@
 // SPDX-FileCopyrightText: 2023 Rot127 <unisono@quyllur.org>
 // SPDX-License-Identifier: LGPL-3.0-only
+
+// NOTE: The Path Sampler should abstract away the iCFG and CFG distinction.
+// The conceptional difference between them is maybe not the smartest descision.
+// Can't judge on this currently.
+//
+// Maybe we want a whole "program control flow graph" in the future.
+// Conceptional changes to the graph logic shouldn't force us to
+// refactor.
