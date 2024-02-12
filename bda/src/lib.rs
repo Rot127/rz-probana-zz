f58b7f954014020dd762880f539ed1830edef3f8
Rot127 <unisono@quyllur.org>
2024-02-11 23:36:19 -0500

    Remove bindgen dependency from BDA since it is provided by probana

--- bda/src/lib.rs
+++ bda/src/lib.rs
@@ -5,8 +5,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_upper_case_globals)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-
 mod abstr_int;
 mod bda;
 pub mod cfg;
--- bda/wrapper.h
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2023 Rot127 <unisono@quyllur.org>
-// SPDX-License-Identifier: LGPL-3.0-only
-
-#include <rz_analysis.h>
-#include <rz_graph.h>
-#include <rz_lib.h>
-#include <rz_core.h>
-
