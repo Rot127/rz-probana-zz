285c8fdce2bef4b89fb5e42d13ab0242e27a9123
Rot127 <unisono@quyllur.org>
2024-06-05 07:00:50 -0500

    Add test binaries for hexagon and x86

--- rzil_abstr/test_bins/build.sh
+++ rzil_abstr/test_bins/build.sh
@@ -1,11 +1,19 @@
 # SPDX-FileCopyrightText: 2024 Rot127 <unisono@quyllur.org>
 # SPDX-License-Identifier: LGPL-3.0-only
 
-echo "Compile binaries"
-clang -c -o icall.o icall.c
-clang -c -o stack_int.o stack_int.c
-clang -c -o stack_mem_arg.o stack_mem_arg.c
-clang -c -o stack_multiple.o stack_multiple.c
-clang -c -o stack_no_param.o stack_no_param.c
-clang -c -o stack_ptr.o stack_ptr.c
-clang -o all stack_*.o all.c
+echo "Compile x86 binaries"
+clang -c -o x86_icall.o icall.c
+clang -c -o x86_stack_int.o stack_int.c
+clang -c -o x86_stack_mem_arg.o stack_mem_arg.c
+clang -c -o x86_stack_multiple.o stack_multiple.c
+clang -c -o x86_stack_no_param.o stack_no_param.c
+clang -c -o x86_stack_ptr.o stack_ptr.c
+
+echo "Compile Hexagon binaries"
+hexagon-unknown-linux-musl-clang -c -O1 -o hexagon_icall.o icall.c
+hexagon-unknown-linux-musl-clang -c -O1 -o hexagon_stack_int.o stack_int.c
+hexagon-unknown-linux-musl-clang -c -O1 -o hexagon_stack_mem_arg.o stack_mem_arg.c
+hexagon-unknown-linux-musl-clang -c -O1 -o hexagon_stack_multiple.o stack_multiple.c
+hexagon-unknown-linux-musl-clang -c -O1 -o hexagon_stack_no_param.o stack_no_param.c
+hexagon-unknown-linux-musl-clang -c -O1 -o hexagon_stack_ptr.o stack_ptr.c
+
