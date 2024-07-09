26c70c1cea9ed228ab1c733e50fcd62be3502024
Rot127 <unisono@quyllur.org>
2024-07-09 08:43:16 -0500

    Move test binaries

--- bda/tests/test_bins/build.sh
+++ bda/tests/test_bins/build.sh
@@ -4,7 +4,9 @@
 echo "Compile x86 binaries"
 clang -c -o x86_unmapped_fcn_in_loop.o unmapped_fcn_in_loop.c
 clang -c -o x86_discover_recurse.o discover_recurse.c
+clang -c -o x86_icall_malloc.o icall_malloc.c
 
 echo "Compile Hexagon binaries"
 hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_unmapped_fcn_in_loop.o unmapped_fcn_in_loop.c
 hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_discover_recurse.o discover_recurse.c
+hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_icall_malloc.o icall_malloc.c
