d8f62bde0e434312fdb6ebff57698428d3d64033
Rot127 <unisono@quyllur.org>
2024-10-01 11:17:27 -0500

    Add integration tests for PPC and ARM

--- bda/tests/integration_tests.rs
+++ bda/tests/integration_tests.rs
@@ -38,6 +38,48 @@ mod test {
     }
 
     #[test]
+    fn test_aaaaPb_arm_ls() {
+        wait_for_exlusive_core!();
+
+        let test_bin = get_rz_test_bin_path()
+            .join("elf")
+            .join("analysis")
+            .join("arm-ls");
+        let core = init_rizin_instance(test_bin.into_os_string().to_str().unwrap());
+        let rz_core = RzCoreWrapper::new(core);
+        rz_core
+            .lock()
+            .unwrap()
+            .set_conf_val("plugins.bda.timeout", "5");
+        rz_core
+            .lock()
+            .unwrap()
+            .set_conf_val("plugins.bda.entries", "0x00011e90");
+        rz_analysis_bda_handler(core, 0, std::ptr::null_mut());
+    }
+
+    #[test]
+    fn test_aaaaPb_ppc_execstack() {
+        wait_for_exlusive_core!();
+
+        let test_bin = get_rz_test_bin_path()
+            .join("elf")
+            .join("analysis")
+            .join("elf-ppc-execstack");
+        let core = init_rizin_instance(test_bin.into_os_string().to_str().unwrap());
+        let rz_core = RzCoreWrapper::new(core);
+        rz_core
+            .lock()
+            .unwrap()
+            .set_conf_val("plugins.bda.timeout", "5");
+        rz_core
+            .lock()
+            .unwrap()
+            .set_conf_val("plugins.bda.skip_questions", "true");
+        rz_analysis_bda_handler(core, 0, std::ptr::null_mut());
+    }
+
+    #[test]
     fn test_aaaaPb_hexagon_test_jmp() {
         wait_for_exlusive_core!();
 
